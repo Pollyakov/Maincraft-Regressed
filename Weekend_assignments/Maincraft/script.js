6eb6915d7b65063c29270814a756f15eddfab347
Ksenia Gountsova <ksusha.net@gmail.com>
2021-02-28 08:22:45 +0200

    functions

--- Weekend_assignments/Maincraft/script.js
+++ Weekend_assignments/Maincraft/script.js
@@ -23,4 +23,48 @@ const pickaxe = document.querySelector('.pickaxe');
 const inventoryTile = document.querySelector('#inventoryTile');
 let inventoryTileClicked = false;
 
+//FUNCTIONS
+const onTileClick = (event) => {
+    let tile = event.target;
+    let tileColor = tile.style.backgroundColor;
+    let inventoryTileColor = inventoryTile.style.backgroundColor;
+    // console.log(tile,tileColor);
+    if (inventoryTileClicked) {
+      tile.style.backgroundColor = inventoryTileColor;
+      inventoryTile.style.backgroundColor = 'black';
+      inventoryTileClicked = false;
+        // if (tileColor == tileColors[0]) {}
+    } else {
+      tile.style.backgroundColor = tileColors[0];
+      inventoryTile.style.backgroundColor = tileColor;
+    }
+};
+
+const drawTile = colorIndex => {
+    let tile = document.createElement('div');
+    tile.classList.add('tile');
+    let tileColor = tileColors[colorIndex];
+    tile.style.backgroundColor = tileColor;
+    tile.style.width = `${dx}px`;
+    tile.style.height = `${dx}px`;
+    spielFeld.appendChild(tile);
+    tile.addEventListener('click',onTileClick);
+}
+
+const init = () => {
+    raster.forEach((item, ind) => {
+       drawTile(item);
+    });
+
+ }
+
+ // EVENT-LISTENERS
+ inventoryTile.addEventListener('click', event => {
+    let inventoryTileColor = inventoryTile.style.backgroundColor;
+    if (inventoryTileColor != 'black') inventoryTileClicked = true;
+});
+
+
+
+
 
