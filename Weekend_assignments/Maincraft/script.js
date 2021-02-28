4a813def25e174380dd60e6b9744f4f351b6ff9c
Ksenia Gountsova <ksusha.net@gmail.com>
2021-02-28 08:16:08 +0200

    variables

--- Weekend_assignments/Maincraft/script.js
+++ Weekend_assignments/Maincraft/script.js
@@ -1,16 +1,26 @@
+'use strict';
 
 //VARIABLES:
-
 const body = document.querySelector('body');
 const nTiles = 20;
 
-
-
+//START PAGE
 const startSelfDelete = () => {
     let start = document.querySelector(".start");
     start.remove();
 };
 const startBtn = document.querySelector(".start-btn");
-console.log(startBtn);
 startBtn.addEventListener("click", startSelfDelete);
+
+//PLAYFIELD
+const playField = document.querySelector('#playField');
+const dx = (playField.clientWidth-12)/nTiles;
+
+//TOOLSBAR
+const axe = document.querySelector('.axe');
+const shovel = document.querySelector('.shovel');
+const pickaxe = document.querySelector('.pickaxe');
+const inventoryTile = document.querySelector('#inventoryTile');
+let inventoryTileClicked = false;
+
 
